0f5bd5cad9f0280d7bd4506c17ccf5dbc7eecae4
AlekseyBagaudinov <bagaudinov01.01@mail.ru>
2026-05-19 15:52:18 +0500

    basic17.js - реализована функция remove_duplicates для удаления дубликатов из массива

--- src/basic/basic17.js
+++ src/basic/basic17.js
@@ -5,5 +5,11 @@
 
 export function remove_duplicates(arr) {
     const unique = [];
+    // Перебираем элементы и добавляем их в массив unique, если их там еще нет
+    for (let i = 0; i < arr.length; i++) {
+        if (!unique.includes(arr[i])) {
+            unique.push(arr[i]);
+        }
+    }
     return unique;
 }
