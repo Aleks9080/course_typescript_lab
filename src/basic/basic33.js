3cb993c110469770d6de45897966be7d70dce643
AlekseyBagaudinov <bagaudinov01.01@mail.ru>
2026-05-19 21:36:21 +0500

    basic33.js - реализована функция zeroRowColumn для зануления строки и столбца матрицы

--- src/basic/basic33.js
+++ src/basic/basic33.js
@@ -3,5 +3,18 @@
 */
 
 export function zeroRowColumn(matrix, row, col) {
-    return matrix;
+  const result = [];
+  // Создаем новую матрицу, зануляя элементы на пересечении строки и столбца
+  for (let i = 0; i < matrix.length; i++) {
+    const newRow = [];
+    for (let j = 0; j < matrix[i].length; j++) {
+      if (i === row || j === col) {
+        newRow.push(0);
+      } else {
+        newRow.push(matrix[i][j]);
+      }
+    }
+    result.push(newRow);
+  }
+  return result;
 }
