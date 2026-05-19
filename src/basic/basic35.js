678c4699230cf5af3cd9154732991c7b73db88e2
AlekseyBagaudinov <bagaudinov01.01@mail.ru>
2026-05-19 22:37:33 +0500

    basic35.js - реализована функция diagonalSum для нахождения суммы главной диагонали матрицы

--- src/basic/basic35.js
+++ src/basic/basic35.js
@@ -1,8 +1,12 @@
 /* 
-	Реализуйте функцию diagonalSum(matrix), которая вычисляет сумму элементов главной диагонали матрицы.
+  Реализуйте функцию diagonalSum(matrix), которая вычисляет сумму элементов главной диагонали матрицы.
 */
 
 export function diagonalSum(matrix) {
-	let sum = 0;
-	return sum;
+  let sum = 0;
+  // Складываем элементы главной диагонали 
+  for (let i = 0; i < matrix.length; i++) {
+    sum += matrix[i][i];
+  }
+  return sum;
 }
