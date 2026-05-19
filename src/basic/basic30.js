35af744e63ecb5e136c5e2aacbfbe6dc4259caeb
AlekseyBagaudinov <bagaudinov01.01@mail.ru>
2026-05-19 20:11:15 +0500

    basic30.js - реализована функция extractUniqueValues для объединения уникальных значений первого массива со вторым массивом

--- src/basic/basic30.js
+++ src/basic/basic30.js
@@ -3,6 +3,19 @@
 */
 
 export function extractUniqueValues(arr1, arr2) {
-    return arr1;
+  const result = [];
+  // Копируем элементы второго массива
+  for (let i = 0; i < arr2.length; i++) {
+    result.push(arr2[i]);
+  }
+  
+  // Добавляем элементы из первого массива, если их еще нет во втором массиве
+  for (let i = 0; i < arr1.length; i++) {
+    if (result.indexOf(arr1[i]) === -1) {
+      result.push(arr1[i]);
+    }
+  }
+  
+  return result;
 }
 
