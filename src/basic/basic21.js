464b9f035bae4beb89be09a467e1a60843ce6bd1
AlekseyBagaudinov <bagaudinov01.01@mail.ru>
2026-05-19 17:12:08 +0500

    basic21.js - реализована функция capitalize_words для преобразования первых букв слов в заглавные

--- src/basic/basic21.js
+++ src/basic/basic21.js
@@ -4,5 +4,15 @@
 */
 
 export function capitalize_words(sentence) {
-	return "";
+  if (!sentence) return "";
+  
+  const words = sentence.split(" ");
+  // Проходим по каждому слову и делаем первую букву заглавной
+  for (let i = 0; i < words.length; i++) {
+    if (words[i].length > 0) {
+      words[i] = words[i][0].toUpperCase() + words[i].slice(1);
+    }
+  }
+  
+  return words.join(" ");
 }
