4562155a82356ad0f0d241aabacbe73142030ddf
AlekseyBagaudinov <bagaudinov01.01@mail.ru>
2026-05-22 11:38:03 +0500

    classes09 - добавлено статическое свойство discountRate и реализован applyDiscount, применяющий скидку ко всем переданным экземплярам Product

--- src/classes/classes09.ts
+++ src/classes/classes09.ts
@@ -3,9 +3,12 @@
 */
 
 export class Product {
-  
+   
   name: string;
   price: number;
+  // статическая ставка скидки, используемая методом applyDiscount
+  static discountRate: number = 0;
+
   constructor(name: string, price: number) {
     this.name = name;
     this.price = price;
@@ -14,6 +17,9 @@ export class Product {
     console.log(`Продукт: ${this.name}, цена: ${this.price}`);
   } 
   static applyDiscount(products: Product[]) {
-  
+    // применяем текущую скидку ко всем переданным товарам
+    for (const product of products) {
+      product.price *= (1 - Product.discountRate);
+    }
   }
 }
