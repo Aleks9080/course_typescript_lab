747e371b3a3e3236da527ec7cf11df65a263ff01
AlekseyBagaudinov <bagaudinov01.01@mail.ru>
2026-05-23 19:35:55 +0500

    thread14 - getUserPostsWithAuthor сначала получает посты пользователя, затем параллельно подгружает авторов через fetchUser для каждого поста и возвращает массив с обогащёнными данными

--- src/thread/thread14.ts
+++ src/thread/thread14.ts
@@ -2,10 +2,19 @@
 	Дописать функцию getUserPostsWithAuthor, которая получает посты пользователя, а затем информацию об авторе каждого поста.
 */
 
-import { fetchUser, fetchUserPosts, type Post } from "./promises";
-import type { User } from "./thread07";
+import { fetchUser, fetchUserPosts, type Post, type User } from "./promises";
 
-export function getUserPostsWithAuthor(userId: number): Promise<Array<Post & { author: User }>> {
-	// TODO: Реализовать цепочку промисов
+export async function getUserPostsWithAuthor(userId: number): Promise<Array<Post & { author: User }>> {
+  // получаем посты пользователя
+  const posts = await fetchUserPosts(userId);
 
+  // для каждого поста подгружаем автора и собираем результат
+  const postsWithAuthors = await Promise.all(
+    posts.map(async (post) => {
+      const author = await fetchUser(post.userId);
+      return { ...post, author };
+    })
+  );
+
+  return postsWithAuthors;
 }
