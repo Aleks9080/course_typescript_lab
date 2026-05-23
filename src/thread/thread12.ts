c8161678d94472ff2bb85603fcc1e53b2e452433
AlekseyBagaudinov <bagaudinov01.01@mail.ru>
2026-05-23 18:30:53 +0500

    thread12 - getUserProfile теперь параллельно запрашивает пользователя, данные и настройки через Promise.all и возвращает полный профиль

--- src/thread/thread12.ts
+++ src/thread/thread12.ts
@@ -5,11 +5,17 @@
 import { fetchUser, fetchUserData, fetchUserSettings } from "./promises";
 import type { User } from "./thread07";
 
-export function getUserProfile(id: number): Promise<{
+export async function getUserProfile(id: number): Promise<{
   user: User;
   data: User;
   settings: { theme: string };
 }> {
-  // TODO: Реализовать параллельное выполнение трех запросов
+  // запускаем все три запроса параллельно
+  const [user, data, settings] = await Promise.all([
+    fetchUser(id),
+    fetchUserData(id),
+    fetchUserSettings(id),
+  ]);
 
+  return { user, data, settings };
 }
